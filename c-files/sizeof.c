62fa115403ac4cd10fcc419dc1cc2b45d5979d71
Eng-Otto <nyongole.otto@gmail.com>
2022-06-13 10:59:44 +0300

    added line to print the size of the pointer p

--- c-files/sizeof.c
+++ c-files/sizeof.c
@@ -8,9 +8,13 @@
 int main(void)
 {
     int n;
+    int *p;
     printf("size of type 'char' on my computer: %lu bytes\n", sizeof(char));
     printf("size of type 'int' on my computer: %lu bytes\n", sizeof(int));
     printf("size of type 'float' on my computer: %lu bytes\n", sizeof(float));
     printf("size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
+
+    printf("print the address of the variable n: %p\n", &n);
+    printf("Size of the pointer: %lu\n", sizeof(p));
     return (0);
 }
