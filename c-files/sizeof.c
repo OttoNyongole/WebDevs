be9efeb2a645a958e848ff3e852adc5116111ee8
Eng-Otto <nyongole.otto@gmail.com>
2022-06-13 11:13:07 +0300

    added line to print the value of variable p

--- c-files/sizeof.c
+++ c-files/sizeof.c
@@ -9,6 +9,9 @@ int main(void)
 {
     int n;
     int *p;
+
+    n = 98;
+    p = &n;
     printf("size of type 'char' on my computer: %lu bytes\n", sizeof(char));
     printf("size of type 'int' on my computer: %lu bytes\n", sizeof(int));
     printf("size of type 'float' on my computer: %lu bytes\n", sizeof(float));
@@ -16,5 +19,7 @@ int main(void)
 
     printf("print the address of the variable n: %p\n", &n);
     printf("Size of the pointer: %lu\n", sizeof(p));
+    printf("The address of the variable 'p': %p\n", &p);
+    printf("The value of varible p: %p\n", p);
     return (0);
 }
