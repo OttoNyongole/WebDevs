f4e37e031f21a3b7d992d53415621f0a249151d6
Eng-Otto <nyongole.otto@gmail.com>
2022-06-30 20:43:48 +0300

    if condition with booleans

--- Python/intro.py
+++ Python/intro.py
@@ -5,3 +5,9 @@ print(msg.lower())
 print(msg.upper())
 print(len(msg)) 
 print(msg.lower() .upper())
+
+door_is_locked = False
+if door_is_locked:
+        print("Mom, open the door!")
+else: 
+    print("The door is open, enter please!")
