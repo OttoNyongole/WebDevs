17f1e0acec30ef34e24f75b883b5ff03d1cefc8e
OttoNyongole <nyongole.otto@gmail.com>
2022-07-05 19:29:11 +0300

    for loop v1

---
+++ Python/pythonMethods.py
@@ -0,0 +1,11 @@
+#for loops
+#used to iterate over a sequences (list, tuple, dictionary, set or string)
+#for loop can execute a set of statements once for each item in a list tuple set ect
+
+#Print each fruit in a list
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
