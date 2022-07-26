5afdab22cbfb9cd6d3c4591408bcaa2b5a7f3ca3
Eng-Otto <nyongole.otto@gmail.com>
2022-07-26 15:43:21 +0300

    added statement if file

---
+++ Python/ifstatement.py
@@ -0,0 +1,22 @@
+var = 1
+if var < 0:
+    print("The value of var is negative")
+else:
+    print("The value of var is positive")
+
+#nested if statement
+score = 100
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 50:
+    print("D")
+elif score >= 35:
+    print("E")
+else:
+    print("FAILED")
+
+
