4f2916df6bb502b82224c719bdc0825bfff9c83a
Eng-Otto <nyongole.otto@gmail.com>
2022-07-26 15:39:45 +0300

    added several files

---
+++ c-files/format_specifier.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  char ch = 'a';
+  char g = 'this is character';
+  char b;
+  printf("This is how to print character %c\n", ch);
+  printf ("%s\n", g);
+  printf(" character for format specofier: %c\n", b);
+
+  return(0);
+}
