e3079225a6f3f71c4695e1e086f4deb98b38169f
OttoNyongole <nyongole.otto@gmail.com>
2022-08-01 18:01:40 +0300

    modifictaion of header files

--- c-files/bultinCommands.c
+++ c-files/bultinCommands.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<readline/readline.h>
-#include<readline/history.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
   
 #define MAXCOM 1000 // max number of letters to be supported
 #define MAXLIST 100 // max number of commands to be supported
