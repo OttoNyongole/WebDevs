1cf25724bb16ba6cc385679cd10a3eda50ff9ddf
OttoNyongole <nyongole.otto@gmail.com>
2022-08-18 08:27:38 +0300

    test file for testing codes before adding to github

--- c-files/test.c
+++ c-files/test.c
@@ -2,5 +2,10 @@
 #include <string.h>
 int main()
 {
-    
+    char* name[35];
+   printf("\nHello, World!. I am learning c, it's so cool ever.");
+   printf("Please enter the string:");
+   scanf("%s", &name);
+   printf("The name is: %s", name);
+   return 0; 
 }
